1c398a450077c56a6ce758e9833141129bf774bf
HaofengLiang <haofengliang.developer@gmail.com>
2024-02-06 20:54:27 -0500

    Add dummy question view.

--- polls/views.py
+++ polls/views.py
@@ -5,3 +5,12 @@ from django.http import HttpResponse, HttpRequest
 
 def index(request: HttpRequest):
     return HttpResponse("Hello, world. You're at the polls index.")
+
+
+def detail(request: HttpRequest, question_id: int):
+    return HttpResponse(f"You're looking at question {question_id}")
+
+
+def results(request: HttpRequest, question_id: int):
+    response = f"You're looking at the results of question {question_id}"
+    return HttpResponse(response)
